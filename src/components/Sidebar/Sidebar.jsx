9ac0c1c7b2eb98b4da15fcf9c24f8566ac8a3cce
MOHAN PANDEY <mohanpandey1299@gmail.com>
2026-06-27 18:28:17 +0530

    Add files via upload

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import logoWhite from "../../assets/logoWhite.png";
 import Languages from "./Languages";
 import Favourites from "./Favourites";
-import { FaGithub } from "react-icons/fa";
 import { MdOutlineMenu } from "react-icons/md";
 import Image from "next/image";
 import Link from "next/link";
@@ -58,16 +57,6 @@ const Sidebar = ({ showNav, setShowNav }) => {
             DMCA
           </p>
         </Link>
-        <a
-          href="https://github.com/himanshu8443/hayasaka"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <p className=" hover:border border-gray-200 p-1 font-medium w-fit rounded cursor-pointer text-sm flex items-center gap-1">
-            <FaGithub />
-            Github
-          </p>
-        </a>
       </div>
     </div>
   );
